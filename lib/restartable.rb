d116be6fd2cc1e72061c3e51b7e852ef0f310847
Ivan Kuchin <tadump+git@gmail.com>
2013-04-02 01:31:30 +0200

    rewritten using traps, killing children gentler but with confidence, changed messages

--- lib/restartable.rb
+++ lib/restartable.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 require 'sys/proctable'
 require 'colored'
 
@@ -6,42 +8,73 @@ class Restartable
     Gem.loaded_specs['restartable'].version.to_s rescue 'DEV'
   end
 
+  WAIT_SIGNALS = [[1, 'INT'], [1, 'INT'], [1, 'INT'], [1, 'TERM'], [5, 'KILL']]
+
   def initialize(options, &block)
     @options, @block = options, block
+    @mutex = Mutex.new
+    synced_trap('INT'){ interrupt! }
+    synced_trap('TERM'){ no_restart!; interrupt! }
     cycle
   end
 
-  def cycle
-    puts '^C to restart, double ^C to stop'.red
-    loop do
-      begin
-        @block.call
-        sleep
-      rescue SignalException
-        begin
-          until children.empty?
-            sleep 1
-            children.each do |child|
-              Process.detach(child.pid)
-              Process.kill('TERM', child.pid)
-            end
-          end
-          unless @stop
-            puts 'Waiting ^C 0.5 second and block death than restart...'.yellow.bold
-            sleep 0.5
-          end
-        rescue SignalException
-          @stop = true
-          puts 'Don\'t restart'.red
-          retry
+  def interrupt!
+    unless @interrupted
+      @interrupted = true
+      Thread.list.each do |thread|
+        unless Thread.current == thread
+          thread.raise SignalException.new('INT')
         end
       end
-      break if @stop
-      puts 'Restarting...'.green
+    else
+      no_restart!
+    end
+  end
+
+  def no_restart!
+    @stop = true
+    puts 'Don\'t restart!'.red.bold
+  end
+
+  def cycle
+    until @stop
+      @interrupted = false
+      puts '^C to restart, double ^C to stop'.green
+      begin
+        @block.call
+        sleep # wait ^C even if block finishes
+      rescue SignalException
+        unless children.empty?
+          puts 'Killing children…'.yellow.bold
+          ripper = Thread.new do
+            WAIT_SIGNALS.each do |time, signal|
+              sleep time
+              puts "…SIG#{signal}…".yellow
+              children.each do |child|
+                Process.kill(signal, child.pid)
+              end
+            end
+          end
+          Process.waitall
+          ripper.terminate
+        end
+        unless @stop
+          puts 'Waiting ^C 0.5 second than restart…'.yellow.bold
+          sleep 0.5
+        end
+      end
     end
   end
 
 private
+
+  def synced_trap(signal, &block)
+    Signal.trap(signal) do
+      Thread.new do
+        @mutex.synchronize(&block)
+      end
+    end
+  end
 
   def children
     pid = Process.pid
