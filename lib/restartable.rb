4db411f6e47fb19d7463d7074c6557f8a8ea87dc
Ivan Kuchin <tadump+git@gmail.com>
2013-08-29 02:17:19 +0200

    sys-proctable does not unify fields, use paid, perp or ppid

--- lib/restartable.rb
+++ lib/restartable.rb
@@ -62,7 +62,7 @@ private
   WAIT_SIGNALS = [[5, 'INT'], [3, 'INT'], [1, 'INT'], [3, 'TERM'], [5, 'KILL']]
 
   def kill_children!
-    until (pids = Sys::ProcTable.ps.select{ |pe| @cpid == pe.pgid }.map(&:pid)).empty?
+    until (pids = children_pids).empty?
       $stderr << "Killing children…\n".yellow.bold
       ripper = Thread.new do
         WAIT_SIGNALS.each do |time, signal|
@@ -83,5 +83,20 @@ private
       end
       ripper.terminate
     end
+  end
+
+  def children_pids
+    Sys::ProcTable.ps.select do |pe|
+      @cpid == case
+      when pe.respond_to?(:pgid)
+        pe.pgid
+      when pe.respond_to?(:pgrp)
+        pe.pgrp
+      when pe.respond_to?(:ppid)
+        pe.ppid
+      else
+        raise 'Can\'t find process group id'
+      end
+    end.map(&:pid)
   end
 end
